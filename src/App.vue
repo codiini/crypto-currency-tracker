69ae31a3692a37767131c63767da2bf0c160592a
Iniubong <iniubongobonguko2018@gmail.com>
2021-03-14 02:16:25 +0100

    made api calls and string formatting

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,62 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <CryptoTable :currencyData="currencyData" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from "@/components/Header";
+import CryptoTable from "@/components/CryptoTable";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Header,
+    CryptoTable,
+  },
+
+  data() {
+    return {
+      currencyData: [],
+    };
+  },
+  mounted() {
+    fetch("https://api.coinlore.net/api/tickers/")
+      .then((response) => response.json())
+      .then((data) => {
+        this.currencyData = data.data.map((currency) => ({
+          ...currency,
+          price: this.formatUsd(currency.price_usd),
+          marketCap: this.formatUsd(currency.market_cap_usd),
+          volume24h: this.formatUsd(currency.volume24),
+          percentChange24h: this.formatPercentages(currency.percent_change_24h),
+          percentChange7d: this.formatPercentages(currency.percent_change_7d),
+          circulatingSupply:
+            this.formatSupply(currency.csupply) + ` ${currency.symbol}`,
+        }));
+      });
+  },
+  methods: {
+    formatUsd(value) {
+      return new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(value);
+    },
+    formatPercentages(value) {
+      return value + "%";
+    },
+    formatSupply(value) {
+      return new Intl.NumberFormat("en-IN", {
+        maximumSignificantDigits: 3,
+      }).format(value);
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: "Poppins", sans-serif;
 }
 </style>
