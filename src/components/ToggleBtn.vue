f68f861e4c4d683c0b6b4413e32e1390204f8cc3
Iniubong <iniubongobonguko2018@gmail.com>
2021-03-14 02:15:32 +0100

    create toggle switch component 4 darkmode function

---
+++ src/components/ToggleBtn.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <input type="checkbox" id="switch" /><label for="switch">Toggle</label>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+input[type="checkbox"] {
+  height: 0;
+  width: 0;
+  visibility: hidden;
+}
+
+label {
+  cursor: pointer;
+  text-indent: -9999px;
+  width: 50px;
+  height: 25px;
+  background: grey;
+  display: block;
+  border-radius: 100px;
+  position: relative;
+}
+
+label:after {
+  content: "";
+  position: absolute;
+  top: 5px;
+  left: 5px;
+  width: 15px;
+  height: 15px;
+  background: #fff;
+  border-radius: 90px;
+  transition: 0.3s;
+}
+
+input:checked + label {
+ @apply bg-blue-200
+}
+
+input:checked + label:after {
+  left: calc(100% - 5px);
+  transform: translateX(-100%);
+}
+
+label:active:after {
+  width: 30px;
+}
+</style>
